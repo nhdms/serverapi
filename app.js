59c565d0e6e4b70c17b5a01464f314126131281f
HungTran <tdh4vn@gmail.com>
2017-12-08 00:45:24 +0700

    x

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ app.use('/data', data);
 app.use('/locations', locations);
 app.get('/check-server', function (req, res) {
   res.json({
-    serverPort: process.env.SERVER_NAME
+    serverName: process.env.SERVER_NAME
   })
 })
 app.get('*', (req, res) => {
